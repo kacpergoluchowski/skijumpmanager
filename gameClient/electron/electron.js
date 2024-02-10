4e57583c7d414b503323a9cb31ae1bbbec9b1767
kacpergoluchowski <kgoluchowski112@gmail.com>
2024-02-10 14:21:35 +0100

    Start project. React, Vite, Electron installed.

---
+++ gameClient/electron/electron.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+ 
+const isDev = process.env.IS_DEV == "true" ? true : false;
+ 
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    autoHideMenuBar: false,
+    resizable: true,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+  });
+ 
+  mainWindow.webContents.setWindowOpenHandler((edata) => {
+    shell.openExternal(edata.url);
+    return { action: "deny" };
+  });
+ 
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../dist/index.html')}`
+  );
+
+  mainWindow.setFullScreen(true);
+  // Open the DevTools.
+  if (isDev) {
+    //mainWindow.webContents.openDevTools();
+  }
+ 
+}
+ 
+ 
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+});
+ 
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
