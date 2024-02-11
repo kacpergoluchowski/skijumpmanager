266195d18ed084508db32e0ad7670e9bf62ffaac
kacpergoluchowski <kgoluchowski112@gmail.com>
2024-02-11 17:20:51 +0100

    add react-router-dom, sass.

--- gameClient/src/main.jsx
+++ gameClient/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import StartPage from './pages/StartPage.jsx'
+import Test from './pages/Test.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <StartPage />
+  },
+  {
+    path: '/test',
+    element: <Test />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
